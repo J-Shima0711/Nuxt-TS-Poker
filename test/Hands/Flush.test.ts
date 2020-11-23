3aa9f2d6ccb18274db1b68ee930b7a0bfc0b1596
Rikuto Yamaguchi <yamaguchirkt@gmail.com>
2020-11-23 15:15:19 +0900

    Fix flush test

--- test/Hands/Flush.test.ts
+++ test/Hands/Flush.test.ts
@@ -2,44 +2,39 @@ import { Flush } from '~/entities/hands/Flush'
 import * as testData from '~/test/TestData'
 
 describe('Flush', () => {
-  test('high card', () => {
+  test('isStraight return false and isFlush return true', () => {
     const hand = new Flush(testData.HIGH_CARD)
-    expect(hand.judge()).toBeFalsy()
-  })
-  test('one pair', () => {
-    const hand = new Flush(testData.ONE_PAIR)
-    expect(hand.judge()).toBeFalsy()
-  })
-  test('two pair', () => {
-    const hand = new Flush(testData.TWO_PAIR)
-    expect(hand.judge()).toBeFalsy()
-  })
-  test('three of a kind', () => {
-    const hand = new Flush(testData.THREE_OF_A_KIND)
-    expect(hand.judge()).toBeFalsy()
-  })
-  test('straight', () => {
-    const hand = new Flush(testData.STRAIGHT)
-    expect(hand.judge()).toBeFalsy()
-  })
-  test('flush', () => {
-    const hand = new Flush(testData.FLUSH)
+    const isStraight = jest
+      .spyOn<any, any>(hand, 'isStraight')
+      .mockReturnValue(false)
+    const isFlush = jest.spyOn<any, any>(hand, 'isFlush').mockReturnValue(true)
+
     expect(hand.judge()).toBeTruthy()
+    expect(isStraight).toHaveBeenCalledTimes(1)
+    expect(isFlush).toHaveBeenCalledTimes(1)
   })
-  test('full house', () => {
-    const hand = new Flush(testData.FULL_HOUSE)
+
+  test('isStraight return false and isFlush return false', () => {
+    const hand = new Flush(testData.HIGH_CARD)
+    const isStraight = jest
+      .spyOn<any, any>(hand, 'isStraight')
+      .mockReturnValue(false)
+    const isFlush = jest.spyOn<any, any>(hand, 'isFlush').mockReturnValue(false)
+
     expect(hand.judge()).toBeFalsy()
+    expect(isStraight).toHaveBeenCalledTimes(1)
+    expect(isFlush).toHaveBeenCalledTimes(1)
   })
-  test('quads', () => {
-    const hand = new Flush(testData.QUADS)
+
+  test('isStraight return true', () => {
+    const hand = new Flush(testData.HIGH_CARD)
+    const isStraight = jest
+      .spyOn<any, any>(hand, 'isStraight')
+      .mockReturnValue(true)
+    const isFlush = jest.spyOn<any, any>(hand, 'isFlush').mockReturnValue(true)
+
     expect(hand.judge()).toBeFalsy()
-  })
-  test('straight flush', () => {
-    const hand = new Flush(testData.STRAIGHT_FLUSH)
-    expect(hand.judge()).toBeFalsy()
-  })
-  test('royal flush', () => {
-    const hand = new Flush(testData.ROYAL_FLUSH)
-    expect(hand.judge()).toBeFalsy()
+    expect(isStraight).toHaveBeenCalledTimes(1)
+    expect(isFlush).toHaveBeenCalledTimes(0)
   })
 })
