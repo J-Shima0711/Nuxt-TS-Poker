f5c5d7623a993ad823ce9a85a1aca4ef25cc8fcc
JunShimanuki <shimanukijun@MacBook-Pro.local>
2020-11-26 17:36:18 +0900

    test

--- pages/PokerGame.vue
+++ pages/PokerGame.vue
@@ -1,64 +1,64 @@
 <template>
   <div class="back-ground">
     <game-board></game-board>
-    <!--    <div class="d-none">-->
-    <!--      <img src="~/assets/board2.png" alt="preload" />-->
-    <!--      <img src="~/assets/body.jpg" alt="preload" />-->
-    <!--      <img src="~/assets/wood.png" alt="preload" />-->
-    <!--      <img :src="'./images/back.png'" alt="preload" />-->
-    <!--      <img :src="'./images/C2.png'" alt="preload" />-->
-    <!--      <img :src="'./images/C3.png'" alt="preload" />-->
-    <!--      <img :src="'./images/C4.png'" alt="preload" />-->
-    <!--      <img :src="'./images/C5.png'" alt="preload" />-->
-    <!--      <img :src="'./images/C6.png'" alt="preload" />-->
-    <!--      <img :src="'./images/C7.png'" alt="preload" />-->
-    <!--      <img :src="'./images/C8.png'" alt="preload" />-->
-    <!--      <img :src="'./images/C9.png'" alt="preload" />-->
-    <!--      <img :src="'./images/C10.png'" alt="preload" />-->
-    <!--      <img :src="'./images/CA.png'" alt="preload" />-->
-    <!--      <img :src="'./images/CJ.png'" alt="preload" />-->
-    <!--      <img :src="'./images/CK.png'" alt="preload" />-->
-    <!--      <img :src="'./images/CQ.png'" alt="preload" />-->
-    <!--      <img :src="'./images/D2.png'" alt="preload" />-->
-    <!--      <img :src="'./images/D3.png'" alt="preload" />-->
-    <!--      <img :src="'./images/D4.png'" alt="preload" />-->
-    <!--      <img :src="'./images/D5.png'" alt="preload" />-->
-    <!--      <img :src="'./images/D6.png'" alt="preload" />-->
-    <!--      <img :src="'./images/D7.png'" alt="preload" />-->
-    <!--      <img :src="'./images/D8.png'" alt="preload" />-->
-    <!--      <img :src="'./images/D9.png'" alt="preload" />-->
-    <!--      <img :src="'./images/D10.png'" alt="preload" />-->
-    <!--      <img :src="'./images/DA.png'" alt="preload" />-->
-    <!--      <img :src="'./images/DJ.png'" alt="preload" />-->
-    <!--      <img :src="'./images/DK.png'" alt="preload" />-->
-    <!--      <img :src="'./images/DQ.png'" alt="preload" />-->
-    <!--      <img :src="'./images/H2.png'" alt="preload" />-->
-    <!--      <img :src="'./images/H3.png'" alt="preload" />-->
-    <!--      <img :src="'./images/H4.png'" alt="preload" />-->
-    <!--      <img :src="'./images/H5.png'" alt="preload" />-->
-    <!--      <img :src="'./images/H6.png'" alt="preload" />-->
-    <!--      <img :src="'./images/H7.png'" alt="preload" />-->
-    <!--      <img :src="'./images/H8.png'" alt="preload" />-->
-    <!--      <img :src="'./images/H9.png'" alt="preload" />-->
-    <!--      <img :src="'./images/H10.png'" alt="preload" />-->
-    <!--      <img :src="'./images/HA.png'" alt="preload" />-->
-    <!--      <img :src="'./images/HJ.png'" alt="preload" />-->
-    <!--      <img :src="'./images/HK.png'" alt="preload" />-->
-    <!--      <img :src="'./images/HQ.png'" alt="preload" />-->
-    <!--      <img :src="'./images/S2.png'" alt="preload" />-->
-    <!--      <img :src="'./images/S3.png'" alt="preload" />-->
-    <!--      <img :src="'./images/S4.png'" alt="preload" />-->
-    <!--      <img :src="'./images/S5.png'" alt="preload" />-->
-    <!--      <img :src="'./images/S6.png'" alt="preload" />-->
-    <!--      <img :src="'./images/S7.png'" alt="preload" />-->
-    <!--      <img :src="'./images/S8.png'" alt="preload" />-->
-    <!--      <img :src="'./images/S9.png'" alt="preload" />-->
-    <!--      <img :src="'./images/S10.png'" alt="preload" />-->
-    <!--      <img :src="'./images/SA.png'" alt="preload" />-->
-    <!--      <img :src="'./images/SJ.png'" alt="preload" />-->
-    <!--      <img :src="'./images/SK.png'" alt="preload" />-->
-    <!--      <img :src="'./images/SQ.png'" alt="preload" />-->
-    <!--    </div>-->
+    <div class="d-none">
+      <img src="~/assets/board2.png" alt="preload" />
+      <img src="~/assets/body.jpg" alt="preload" />
+      <img src="~/assets/wood.png" alt="preload" />
+      <img :src="'./images/back.png'" alt="preload" />
+      <img :src="'./images/C2.png'" alt="preload" />
+      <img :src="'./images/C3.png'" alt="preload" />
+      <img :src="'./images/C4.png'" alt="preload" />
+      <img :src="'./images/C5.png'" alt="preload" />
+      <img :src="'./images/C6.png'" alt="preload" />
+      <img :src="'./images/C7.png'" alt="preload" />
+      <img :src="'./images/C8.png'" alt="preload" />
+      <img :src="'./images/C9.png'" alt="preload" />
+      <img :src="'./images/C10.png'" alt="preload" />
+      <img :src="'./images/CA.png'" alt="preload" />
+      <img :src="'./images/CJ.png'" alt="preload" />
+      <img :src="'./images/CK.png'" alt="preload" />
+      <img :src="'./images/CQ.png'" alt="preload" />
+      <img :src="'./images/D2.png'" alt="preload" />
+      <img :src="'./images/D3.png'" alt="preload" />
+      <img :src="'./images/D4.png'" alt="preload" />
+      <img :src="'./images/D5.png'" alt="preload" />
+      <img :src="'./images/D6.png'" alt="preload" />
+      <img :src="'./images/D7.png'" alt="preload" />
+      <img :src="'./images/D8.png'" alt="preload" />
+      <img :src="'./images/D9.png'" alt="preload" />
+      <img :src="'./images/D10.png'" alt="preload" />
+      <img :src="'./images/DA.png'" alt="preload" />
+      <img :src="'./images/DJ.png'" alt="preload" />
+      <img :src="'./images/DK.png'" alt="preload" />
+      <img :src="'./images/DQ.png'" alt="preload" />
+      <img :src="'./images/H2.png'" alt="preload" />
+      <img :src="'./images/H3.png'" alt="preload" />
+      <img :src="'./images/H4.png'" alt="preload" />
+      <img :src="'./images/H5.png'" alt="preload" />
+      <img :src="'./images/H6.png'" alt="preload" />
+      <img :src="'./images/H7.png'" alt="preload" />
+      <img :src="'./images/H8.png'" alt="preload" />
+      <img :src="'./images/H9.png'" alt="preload" />
+      <img :src="'./images/H10.png'" alt="preload" />
+      <img :src="'./images/HA.png'" alt="preload" />
+      <img :src="'./images/HJ.png'" alt="preload" />
+      <img :src="'./images/HK.png'" alt="preload" />
+      <img :src="'./images/HQ.png'" alt="preload" />
+      <img :src="'./images/S2.png'" alt="preload" />
+      <img :src="'./images/S3.png'" alt="preload" />
+      <img :src="'./images/S4.png'" alt="preload" />
+      <img :src="'./images/S5.png'" alt="preload" />
+      <img :src="'./images/S6.png'" alt="preload" />
+      <img :src="'./images/S7.png'" alt="preload" />
+      <img :src="'./images/S8.png'" alt="preload" />
+      <img :src="'./images/S9.png'" alt="preload" />
+      <img :src="'./images/S10.png'" alt="preload" />
+      <img :src="'./images/SA.png'" alt="preload" />
+      <img :src="'./images/SJ.png'" alt="preload" />
+      <img :src="'./images/SK.png'" alt="preload" />
+      <img :src="'./images/SQ.png'" alt="preload" />
+    </div>
   </div>
 </template>
 
@@ -75,70 +75,70 @@ export default Vue.extend({
       text: '' as string,
     }
   },
-  beforeCreate() {
-    const sources: string[] = [
-      './images/back.png',
-      './images/C2.png',
-      './images/C3.png',
-      './images/C4.png',
-      './images/C5.png',
-      './images/C6.png',
-      './images/C7.png',
-      './images/C8.png',
-      './images/C9.png',
-      './images/C10.png',
-      './images/CA.png',
-      './images/CJ.png',
-      './images/CK.png',
-      './images/CQ.png',
-      './images/D2.png',
-      './images/D3.png',
-      './images/D4.png',
-      './images/D5.png',
-      './images/D6.png',
-      './images/D7.png',
-      './images/D8.png',
-      './images/D9.png',
-      './images/D10.png',
-      './images/DA.png',
-      './images/DJ.png',
-      './images/DK.png',
-      './images/DQ.png',
-      './images/H2.png',
-      './images/H3.png',
-      './images/H4.png',
-      './images/H5.png',
-      './images/H6.png',
-      './images/H7.png',
-      './images/H8.png',
-      './images/H9.png',
-      './images/H10.png',
-      './images/HA.png',
-      './images/HJ.png',
-      './images/HK.png',
-      './images/HQ.png',
-      './images/S2.png',
-      './images/S3.png',
-      './images/S4.png',
-      './images/S5.png',
-      './images/S6.png',
-      './images/S7.png',
-      './images/S8.png',
-      './images/S9.png',
-      './images/S10.png',
-      './images/SA.png',
-      './images/SJ.png',
-      './images/SK.png',
-      './images/SQ.png',
-    ]
-    const images = []
-    const preload = (): void => {
-      sources.forEach((src, i) => {
-        images[i] = new Image().src = src
-      })
-    }
-    preload()
-  },
+  // beforeCreate() {
+  //   const sources: string[] = [
+  //     './images/back.png',
+  //     './images/C2.png',
+  //     './images/C3.png',
+  //     './images/C4.png',
+  //     './images/C5.png',
+  //     './images/C6.png',
+  //     './images/C7.png',
+  //     './images/C8.png',
+  //     './images/C9.png',
+  //     './images/C10.png',
+  //     './images/CA.png',
+  //     './images/CJ.png',
+  //     './images/CK.png',
+  //     './images/CQ.png',
+  //     './images/D2.png',
+  //     './images/D3.png',
+  //     './images/D4.png',
+  //     './images/D5.png',
+  //     './images/D6.png',
+  //     './images/D7.png',
+  //     './images/D8.png',
+  //     './images/D9.png',
+  //     './images/D10.png',
+  //     './images/DA.png',
+  //     './images/DJ.png',
+  //     './images/DK.png',
+  //     './images/DQ.png',
+  //     './images/H2.png',
+  //     './images/H3.png',
+  //     './images/H4.png',
+  //     './images/H5.png',
+  //     './images/H6.png',
+  //     './images/H7.png',
+  //     './images/H8.png',
+  //     './images/H9.png',
+  //     './images/H10.png',
+  //     './images/HA.png',
+  //     './images/HJ.png',
+  //     './images/HK.png',
+  //     './images/HQ.png',
+  //     './images/S2.png',
+  //     './images/S3.png',
+  //     './images/S4.png',
+  //     './images/S5.png',
+  //     './images/S6.png',
+  //     './images/S7.png',
+  //     './images/S8.png',
+  //     './images/S9.png',
+  //     './images/S10.png',
+  //     './images/SA.png',
+  //     './images/SJ.png',
+  //     './images/SK.png',
+  //     './images/SQ.png',
+  //   ]
+  //   const images = []
+  //   const preload = (): void => {
+  //     sources.forEach((src, i) => {
+  //       images[i] = new Image().src = src
+  //     })
+  //   }
+  //   preload()
+  // },
 })
 </script>
 
